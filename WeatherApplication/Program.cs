70d06c2b0e995b9483f22643bc07ba1555c39fbf
Swaleh Abdulrehman <swaleh.abdulrehman@ihsmarkit.com>
2023-12-07 21:17:04 +0000

    update to program.cs to include swagger config

--- WeatherApplication/Program.cs
+++ WeatherApplication/Program.cs
@@ -1,36 +1,34 @@
-
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using WeatherApplication.Services;
 
-namespace WeatherApplication
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddScoped<IWeatherService, WeatherService>();
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Weather API", Version = "v1" });
+        });
 
-            // Add services to the container.
-            builder.Services.AddScoped<IWeatherService,WeatherService>();
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        var app = builder.Build();
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Weather API V1"));
         }
+
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
     }
 }
