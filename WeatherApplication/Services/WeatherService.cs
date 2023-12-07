c936599ef36fed33d9be22fc19a430fc90ff4b33
Swaleh Abdulrehman <swaleh.abdulrehman@ihsmarkit.com>
2023-12-07 20:30:15 +0000

    switched from dateOnly to dateTime to resolve error

--- WeatherApplication/Services/WeatherService.cs
+++ WeatherApplication/Services/WeatherService.cs
@@ -1,15 +1,17 @@
 ﻿namespace WeatherApplication.Services
-{    public class WeatherService : IWeatherService
+{
+    public class WeatherService : IWeatherService
     {
         private static readonly string[] Summaries = new[]
-{
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
         public IEnumerable<WeatherForecast> GetWeatherForecasts()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Date = DateTime.Now.AddDays(index),  // Use DateTime directly
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
